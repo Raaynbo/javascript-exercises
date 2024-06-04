40bcb55dc0fbc29065a7178802c50c822034da72
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-04 15:01:08 +0200

    finished ex-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, count) {
+  let response = "";
+  if (count < 0){
+    return "ERROR";
+  }
+  for (i = 0; i < count; i++){
+    response += text;
+  }
+  return response;
 };
 
 // Do not edit below this line
