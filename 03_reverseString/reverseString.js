b1250622b77bef36350cd328ee735da3f6304cd0
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-04 15:25:48 +0200

    ex3, test 1 passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  //get text to reverse
+  //store text into an empty string var called original
+  let original = text;
+  //create an empty string var called response
+  let response = "";
+  //get text length
+  let length = text.length;
+  //make a loop to explore text from last to first character
+  for (i = length-1; i >= 0; i--){
+    response += original[i];
+    console.log(original[i]);
+  }
+  //return response
+  return response;
 
 };
 
