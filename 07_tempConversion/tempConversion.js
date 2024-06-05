0d9f8b61926d888a31a39aebfad9d2286473d0ba
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-05 22:03:14 +0200

    ex07, first test for convertToCelsius Passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // create an integer variable init with 0 called response
+  let response = 0;
+  // substract the passed temp by 32 and store it in response
+  // multiply response by 5
+  // divide response by 9
+  response = ((temp - 32) *5)/9;
+  // round response
+  // return response
+  return response;
 };
 
 const convertToFahrenheit = function() {
