b29231a77778ec90c53afa3b70c2d011b9881a1e
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-05 21:50:13 +0200

    ex06, three first test passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // create a boolean variable set to false called response
+  let response = false;
+  // check if year is divisible by 4 and by 400
+  if (year % 4 == 0 && ((year % 100 != 0)|| (year%100 == 0 && year%400==0)) ){
+    
+    response = true;
+  }
+  // assign true to response when it's divisible otherwise assign false
+  // return response
+  return response;
 };
 
 // Do not edit below this line
