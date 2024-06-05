e0e4b0015f55d38e12c19296e200dcc5b6c99a9b
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-05 19:40:23 +0200

    ex04, first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+  //store arguments in array called argsArray
+  let args = [];
+  if (arguments.length > 2){
+    for (let i = 1; i <= arguments.length-1 ; i++){
+      args.push(arguments[i]);
+    }
+  }else{
+      args = arguments[1];
+  }
+  if (Array.isArray(args) === true ){
+    
+  } else {
+    let idx = array.indexOf(args);
+    
+    array.splice(idx,1);
+  }
+  console.log(array);
+  return array;
 };
 
 // Do not edit below this line
