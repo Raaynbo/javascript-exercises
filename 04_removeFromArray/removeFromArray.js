1f17b0c1ad515a3ae4a56452ebb550d08710d774
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-05 20:29:11 +0200

    ex04, third test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,20 @@
 const removeFromArray = function(array) {
+    let toDel = [];
     for (let i = 1; i <= arguments.length-1 ; i++){
-      let idx = array.indexOf(arguments[i]);
-      array.splice(idx,1);
+      array.map((el, index) => {
+        if (el === arguments[i]){
+          toDel.push(index);
+        }
+      });
     }
+  toDel = toDel.sort(function(a, b){return b-a});
+  console.log("index to del "+ toDel);
+  for (let i= 0; i <=toDel.length-1; i++){
+    array.splice(toDel[i],1);
+    console.log(toDel[i-1]);
+  }
   
+  console.log(array);
   return array;
 };
 
