63d2e8a24c1c7223da5258b0b663cbe28844ecab
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-21 16:31:17 +0200

    ex10: finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  // calculate fibonacci value, using an integer argument called "n". 
+  // to calculate the n-th number of the fibonacci value
+  // we have to determine the fibonacci value of each of the precedent value
+  let nInt = n;
+  if (typeof(n) === "string"){
+    nInt = parseInt(n);
+  }
+  if (nInt < 0){
+    return "OOPS";
+  }else if ( nInt === 0){
+    return 0;
+  }else if (nInt === 1){
+    return 1;
+  }
+  let result = fibonacci(n-1) + fibonacci(n-2);
+  return result;
 };
 
 // Do not edit below this line
