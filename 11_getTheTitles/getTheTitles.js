7f503d26fb048d31634e89c23b0c8252635104ce
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-21 17:20:55 +0200

    ex11: finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(book_array) {
+  // an array of books[title, author] is passed as an arg called "book_array"
+  // create an array variable called titles , titles will be empty at first
+  // explore book_array and get titles name into titles
 
+  const titles = [];
+  book_array.map(book => titles.push(book.title));
+  return titles;
 };
 
 // Do not edit below this line
