b7f4fafb1b2b3bb649b76a7c20d55f2a1624940f
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-23 01:47:35 +0200

    ex12: finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
 const findTheOldest = function(ppleArray) {
   const result = ppleArray.sort((ppleA, ppleB) => {
+    if (ppleB.yearOfDeath === undefined){
+      ppleB.yearOfDeath = new Date().getFullYear();
+    }
     return (ppleA.yearOfDeath - ppleA.yearOfBirth) - (ppleB.yearOfDeath - ppleB.yearOfBirth);
   }).pop();
   return result;
--- 12_findTheOldest/findTheOldest.spec.js
+++ 12_findTheOldest/findTheOldest.spec.js
@@ -6,22 +6,22 @@ describe('findTheOldest', () => {
       {
         name: "Carly",
         yearOfBirth: 1942,
-        yearOfDeath: 1970,
+        yearOfDeath: 1970,// => 28ans
       },
       {
         name: "Ray",
         yearOfBirth: 1962,
-        yearOfDeath: 2011,
+        yearOfDeath: 2011, // => 49 ans
       },
       {
         name: "Jane",
         yearOfBirth: 1912,
-        yearOfDeath: 1941,
+        yearOfDeath: 1941, // => 29ans
       },
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if yearOfDeath field is undefined on a non-oldest person', () => {
+  test('finds the oldest person if yearOfDeath field is undefined on a non-oldest person', () => {
     const people = [
       {
         name: "Carly",
@@ -40,7 +40,7 @@ describe('findTheOldest', () => {
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if yearOfDeath field is undefined for the oldest person', () => {
+  test('finds the oldest person if yearOfDeath field is undefined for the oldest person', () => {
     const people = [
       {
         name: "Carly",
