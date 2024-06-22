3bf133f99fa73509ddb5db75fc445edbb507f515
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-23 01:37:45 +0200

    ex12: first test passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ppleArray) {
+  const result = ppleArray.sort((ppleA, ppleB) => {
+    return (ppleA.yearOfDeath - ppleA.yearOfBirth) - (ppleB.yearOfDeath - ppleB.yearOfBirth);
+  }).pop();
+  return result;
 };
 
 // Do not edit below this line
